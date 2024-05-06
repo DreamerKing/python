de60be21145c9ecf628e6811e0aeaa52516ac7e0
Dreamer King <dreamerking@live.cn>
2024-05-06 11:34:30 +0800

    oop

--- python/c2f.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-f = 0
-print("   F   to   C")
-while f <= 250:
-    c = (f - 32) / 1.8
-    print("{:5d}   {:7.2f}".format(f, c))
-    f += 25
--- python/swap.py
+++
@@ -1,7 +0,0 @@
-a, b = 25, 54
-print(a, b)
-a, b = b, a
-print(a, b)
-data = ("Dreamer", "China", "Python")
-name, country, language = data
-print(name, country, language)
